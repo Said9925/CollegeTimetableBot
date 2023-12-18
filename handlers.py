54965a2499ee256f43c4726a69f0c6ea83b94cab
user9925 <tsamaraevv@mail.ru>
2023-12-19 01:44:04 +0300

    added logging to handlers

--- handlers.py
+++ handlers.py
@@ -1,12 +1,24 @@
 import os
+import logging
+
 
 from dotenv import load_dotenv
 import telebot
 
 
-from config import MESSAGE, MESSAGE_GROUP
+from config import MESSAGE, MESSAGE_GROUP, BUTTON_DAY_TEXT, BUTTON_WEEK_TEXT
 from html_parser import day_schedule, week_schedule
 from utils import get_keyboard
+
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    encoding='utf-8',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 load_dotenv()
@@ -20,6 +32,7 @@ def start(message):
     Отправляет приветственное сообщение и клавиатуру с кнопками выбора расписания.
     """
     bot.send_message(message.chat.id, MESSAGE, reply_markup=get_keyboard())
+    logger.info(f"Бот запущен - {message.chat.username}")
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'schedule_day')
@@ -30,6 +43,7 @@ def schedule_day(call):
     """
     bot.send_message(call.message.chat.id, MESSAGE_GROUP)
     bot.register_next_step_handler(call.message, process_group_day)
+    logger.info(f'Пользователь |{call.message.chat.username}| нажал на кнопку |{BUTTON_DAY_TEXT}')
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'schedule_week')
@@ -40,7 +54,7 @@ def schedule_week(call):
     """
     bot.send_message(call.message.chat.id, MESSAGE_GROUP)
     bot.register_next_step_handler(call.message, process_group_week)
-
+    logger.info(f'Пользователь |{call.message.chat.username}| нажал на кнопку |{BUTTON_WEEK_TEXT}')
 
 def process_group_day(message):
     """
@@ -48,7 +62,7 @@ def process_group_day(message):
     Отправляет расписание на день и клавиатуру с кнопками выбора расписания.
     """
     bot.send_message(message.chat.id, day_schedule(message.text), reply_markup=get_keyboard())
-
+    logger.info(f"Бот проверяет корректность названия группы веденной пользователем |{message.chat.username} | группа - {message.text} | {BUTTON_DAY_TEXT}")
 
 def process_group_week(message):
     """
@@ -56,3 +70,4 @@ def process_group_week(message):
     Отправляет расписание на неделю и клавиатуру с кнопками выбора расписания.
     """
     bot.send_message(message.chat.id, week_schedule(message.text), reply_markup=get_keyboard())
+    logger.info(f"Бот проверяет корректность названия группы веденной пользователем |{message.chat.username} | группа - {message.text} | {BUTTON_WEEK_TEXT}")
